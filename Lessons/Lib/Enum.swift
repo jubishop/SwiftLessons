2e4b77a4d324ccb90e3a2ef878757162259e5646
Justin Bishop <jubishop@gmail.com>
2020-10-18 20:48:24 -0700

    notes in Enum about other func

--- Lessons/Lib/Enum.swift
+++ Lessons/Lib/Enum.swift
@@ -1,5 +1,8 @@
 import SwiftUI
 
+// See https://stackoverflow.com/questions/59295206/how-do-you-use-enumerated-with-foreach-in-swiftui/63145650#63145650
+// use func enumeratedArray() ?
+
 func EnumList<T: Identifiable, V: View>(_ ary: Array<T>,
                                         block:@escaping (Int, T) -> V) ->
     List<Never, ForEach<Array<(offset: Int, element: T)>, T.ID, HStack<V>>> {
@@ -9,7 +12,7 @@ func EnumList<T: Identifiable, V: View>(_ ary: Array<T>,
 }
 
 func EnumForEach<T: Identifiable, V: View>(_ ary: Array<T>,
-                                        block:@escaping (Int, T) -> V) ->
+                                           block:@escaping (Int, T) -> V) ->
     ForEach<Array<(offset: Int, element: T)>, T.ID, V> {
   return ForEach(Array(ary.enumerated()), id: \.element.id) { idx, item in
     block(idx, item)
