372d6d321460355ef881eeb95d768645606c734d
Justin Bishop <jubishop@gmail.com>
2020-10-19 15:45:38 -0700

    ok

--- Lessons/Lib/Enum.swift
+++ Lessons/Lib/Enum.swift
@@ -1,8 +1,5 @@
 import SwiftUI
 
-// See https://stackoverflow.com/questions/59295206/how-do-you-use-enumerated-with-foreach-in-swiftui/63145650#63145650
-// use func enumeratedArray() ?
-
 func EnumList<T: Identifiable, V: View>(_ ary: Array<T>,
                                         block:@escaping (Int, T) -> V) ->
     List<Never, ForEach<Array<(offset: Int, element: T)>, T.ID, HStack<V>>> {
