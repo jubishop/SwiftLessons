e37f0d270ba2b1f49d98c796d4046a0513129e5e
Justin Bishop <jubishop@gmail.com>
2024-10-01 12:23:12 -0700

    adding WeSplit practice project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Justin Bishop on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
