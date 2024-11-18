e68a305b9c7ca41dbe4f1b818deec2ae9ba3f5aa
Justin Bishop <jubishop@gmail.com>
2024-11-18 10:57:09 -0800

    adding FlashZilla

---
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -0,0 +1,42 @@
+// Copyright Justin Bishop, 2024
+
+import SwiftUI
+
+extension View {
+  func stacked(at position: Int, in total: Int) -> some View {
+    let offset = Double(total - position)
+    return self.offset(y: offset * 10)
+  }
+}
+
+struct ContentView: View {
+  @State private var cards = [Card](repeating: .example, count: 10)
+
+  var body: some View {
+    ZStack {
+      Image(.background)
+        .resizable()
+        .ignoresSafeArea()
+      VStack {
+        ZStack {
+          ForEach(0..<cards.count, id: \.self) { index in
+            CardView(card: cards[index]) {
+              withAnimation {
+                removeCard(at: index)
+              }
+            }
+            .stacked(at: index, in: cards.count)
+          }
+        }
+      }
+    }
+  }
+
+  func removeCard(at index: Int) {
+    cards.remove(at: index)
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,12 @@
+// Copyright Justin Bishop, 2024 
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
